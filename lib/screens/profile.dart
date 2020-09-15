2d1fec2d85ecb733be536b48ccdde1fda041d92e
sesakaaji19137 <sesaka.aji19137@student.unsika.ac.id>
2020-09-15 12:39:29 +0700

    create profile screen

---
+++ lib/screens/profile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProfileDetail extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<ProfileDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text('Profile')),
+
+      
+    );
+  }
+}
