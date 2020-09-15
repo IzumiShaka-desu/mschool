d22752b5dedd15ab5e7c49f9dc0a73150c071e78
sesakaaji19137 <sesaka.aji19137@student.unsika.ac.id>
2020-09-15 12:15:37 +0700

    remove unused file

--- lib/dummies.dart
+++
@@ -1,4 +0,0 @@
-import 'models/school.dart';
-
-List<School> schools =
-    List.generate(12, (index) => School(npsn: index, name: "sekolah $index",desc: "ini sekolah ke $index",lat: 12312,long: 1233));
